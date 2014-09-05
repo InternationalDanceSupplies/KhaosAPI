79091d94c19a18f56c5ae36b774683b9c2b164e1
Jon Matthews <joncarlmatthews@gmail.com>
2014-09-05 15:25:58 +0100

    API refactor with extension in mind

--- Client.php
+++ Client.php
@@ -6,8 +6,7 @@ namespace KhaosAPI
     {
         private $_soapClient = null;
 
-        private $_callers = array('\\KhaosAPI\\Caller\\ExportStock',
-                                                '\\KhaosAPI\\Caller\\GetVersion');
+        private $_callers = array();
 
         public function __construct(\SoapClient $soapClient)
         {
@@ -26,35 +25,56 @@ namespace KhaosAPI
 
         public function __call($className, $args)
         {
+            $caller = null;
+
             $className = ucfirst($className);
 
-            foreach($this->getCallers() as $fqcn){
+            $internalClassFileName = __DIR__ 
+                                        . DIRECTORY_SEPARATOR 
+                                        . 'Caller' 
+                                        . DIRECTORY_SEPARATOR 
+                                        . $className 
+                                        . '.php';
 
-                if (preg_match('/' . $className . '$/', $fqcn)){
+            if (is_file($internalClassFileName)){
 
-                    $caller = new $fqcn;
+                $caller = '\\KhaosAPI\\Caller\\' . $className;
 
-                    // Set the client.
-                    if ( (isset($args[1]))
-                            && ($args[1] instanceof \SoapClient) ){
-                        $caller->setClient($args[1]);
-                    }else{
-                        $caller->setClient($this->getClient());
+            }else{
+
+                foreach($this->getCallers() as $fqcn){
+
+                    if (preg_match('/' . $className . '$/', $fqcn)){
+
+                        $caller = $fqcn;
+                        break;
                     }
-
-                    // Set the arguments.
-                    if (isset($args[0])){
-                        $caller->setArgs($args[0]);
-                    }
-
-                    // Call the endpoint.
-                    return $caller->run();
-
-                    break;
                 }
             }
 
-            throw new Exception('Caller ' . $className . ' not found');
+            if (!is_null($caller)){
+
+                $caller = new $caller;
+
+                // Set the client.
+                if ( (isset($args[1]))
+                        && ($args[1] instanceof \SoapClient) ){
+                    $caller->setClient($args[1]);
+                }else{
+                    $caller->setClient($this->getClient());
+                }
+
+                // Set the arguments.
+                if (isset($args[0])){
+                    $caller->setArgs($args[0]);
+                }
+
+                // Call the endpoint.
+                return $caller->run();
+
+            }else{
+                throw new Exception('Caller ' . $className . ' not found');
+            }
         }
     }
 }
